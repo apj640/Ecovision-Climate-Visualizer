57b9cd3f5d5c45961f4af585bf9d26d84c1c378e
apj640 <apjermain@gmail.com>
2025-10-23 11:01:08 -0700

    Initialize sqla

--- backend/app.py
+++ backend/app.py
@@ -2,20 +2,28 @@
 # This file contains basic Flask setup code to get you started.
 # You may opt to use FastAPI or another framework if you prefer.
 
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_cors import CORS
-from flask_mysqldb import MySQL
 import os
+from database import db
+
+
+def build_conn_str():
+    """Build the MySQL connection string from environment variables."""
+    host = os.environ.get("PG_HOST", "127.0.0.1")
+    user = os.environ.get("PG_USER", "postgres")
+    password = os.environ.get("PG_PASSWORD", "postgres")
+    db = os.environ.get("DB", "postgres")
+    return f"postgresql+psycopg2://{user}:{password}@{host}/{db}"
+
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-# MySQL Configuration
-app.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST", "localhost")
-app.config["MYSQL_USER"] = os.environ.get("MYSQL_USER", "root")
-app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD", "")
-app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB", "climate_data")
-mysql = MySQL(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = build_conn_str()
+
+db.init_app(app)
+db.create_all()
 
 
 if __name__ == "__main__":
