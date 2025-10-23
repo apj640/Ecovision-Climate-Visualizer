706933baf9ca9624a18b5823564d1eb26f3b7396
apj640 <apjermain@gmail.com>
2025-10-23 11:13:11 -0700

    Use app ctx for sqla init

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from flask import Flask
 from flask_cors import CORS
 import os
 from database import db
+import models
 
 
 def build_conn_str():
@@ -22,8 +23,9 @@ CORS(app)  # Enable CORS for all routes
 
 app.config["SQLALCHEMY_DATABASE_URI"] = build_conn_str()
 
-db.init_app(app)
-db.create_all()
+with app.app_context():
+    db.init_app(app)
+    db.create_all()
 
 
 if __name__ == "__main__":
